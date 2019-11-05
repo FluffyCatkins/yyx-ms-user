a4c600438ee32be3a7de3f3e0f5ebe44c654833a
FluffyCatkins <yangxuxiansheng@outlook.com>
2019-11-05 15:22:12 +0800

    配置feign

--- src/main/java/cn/yyx/msuser/controller/UserController.java
+++ src/main/java/cn/yyx/msuser/controller/UserController.java
@@ -3,7 +3,6 @@ package cn.yyx.msuser.controller;
 import cn.yyx.msuser.domain.entity.User;
 import cn.yyx.msuser.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +15,7 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/findById/{id}")
-    public User findById(@PathVariable Integer id){
+    public User findById(@PathVariable("id") Integer id){
         return this.userService.findById(id);
     }
 
